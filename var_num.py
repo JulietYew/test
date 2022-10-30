0b07b358cfd841e043fd1ef7742025ff28cc9f57
JulietYew <53115967+JulietYew@users.noreply.github.com>
2022-10-30 02:08:30 +0100

    this is our project initial state

---
+++ var_num.py
@@ -0,0 +1,39 @@
+#a name and and the printing the name in lowercase, uppercase and title case
+name = "juliet"
+print(f"{name.lower()}")
+print(f"{name.upper()}")
+print(f"{name.title()}")# a message to print Hello World!
+print("Hello World!")
+
+# adding a variable to print Hello World!
+message = "Hello World!"
+print(message)
+
+#changing case in a string with methods
+name = "okafor juliet"
+# the title() method changes the first letter of every word to capital letter
+print(name.title())
+
+#using variables in strings
+first_name = "juliet"
+last_name = "okafor"
+#the f string is used to compose complete messages using the information associated with the variable
+print(f"Hello, {first_name.title()} {last_name.title()}!")
+
+#using a variable to represent a name and sending a message to that  person
+name = "juliet"
+print(f"Hello {name.title()}, would you like to learn some python today?")
+
+#using a variable to represent
+
+#famous quote
+author = "George Elliot"
+print(f'{author} once said, "It is never too late to be what you might have been."')
+
+#numbers
+# writing addition, subtraction, multiplication and division operations that will result to the number 8
+print(5+3)
+print(10-2)
+print(4 * 2)
+print(16/2)
+
